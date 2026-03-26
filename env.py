1d7f8646fc38869427096a7c9c9c02afa6afca40
DominiqueFre <dominiquereade@gmail.com>
2026-03-26 08:33:00 +0000

    python version

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 
 os.environ.setdefault(
     'DATABASE_URL',
-    'postgresql://neondb_owner:npg_JIUSuD8xrRK9@ep-young-pine-agrqbf5r.c-2.eu-central-1.aws.neon.tech/tray_rice_cape_659667'
+    'postgresql://neondb_owner:npg_JIUSuD8xrRK9@ep-young-pine-agrqbf5r.c-2.eu-central-1.aws.neon.tech/tilt_crisp_even_682048'
 )
 
 os.environ.setdefault(
