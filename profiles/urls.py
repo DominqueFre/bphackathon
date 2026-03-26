1645cc609411f2472366b05e264cfa6fe2b78994
TeasDale <jordanjohnteasdale@gmail.com>
2026-03-26 16:53:37 +0000

    restructured htmls with bootstrap. fixed missing env.py file

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import profile_upsert, add_bp_reading, bp_reading_list, edit_bp_reading, delete_bp_reading
+from .views import (
+    profile_detail,
+    profile_upsert,
+    add_bp_reading,
+    bp_reading_list,
+    edit_bp_reading,
+    delete_bp_reading,
+)
 
 urlpatterns = [
-    path("profile/", profile_upsert, name="profile"),
+    path("profile/", profile_detail, name="profile"),
+    path("profile/edit/", profile_upsert, name="profile_edit"),
+
     path("reading/add/", add_bp_reading, name="add_bp_reading"),
     path("readings/", bp_reading_list, name="bp_reading_list"),
     path("reading/<int:pk>/edit/", edit_bp_reading, name="edit_bp_reading"),
